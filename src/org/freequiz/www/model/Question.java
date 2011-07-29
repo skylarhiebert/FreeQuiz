9681a73f09fda924fedc99dabb2ebe9b1ce17c77
Skylar Hiebert <skylar.hiebert@freequiz.org>
2011-07-28 17:00:59 -0700

    Added Copyright headers to all files.

--- src/org/freequiz/www/model/Question.java
+++ src/org/freequiz/www/model/Question.java
@@ -1,15 +1,13 @@
-/*************************************************************************************************
+/*******************************************************************************
  * Copyright (c) 2011 Skylar Hiebert.
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
- * software and associated documentation files (the "Software"), to deal in the Software 
- * without restriction, including without limitation the rights to use, copy, modify, 
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
- * permit persons to whom the Software is furnished to do so, subject to the following 
- * conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all 
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+ * the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
  * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
@@ -20,8 +18,7 @@
  * 
  * Contributors:
  *     Skylar Hiebert - initial API and implementation
- *************************************************************************************************/
-
+ ******************************************************************************/
 package org.freequiz.www.model;
 
 import javax.persistence.*;
